ea3f709bef382dc610b08c790991ca7c2cfd5580
c0mm0s <multicolorr847@gmail.com>
2026-06-28 12:45:58 +0330

    .

--- Arsha/settings/prod.py
+++ Arsha/settings/prod.py
@@ -2,10 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "domain.com",
-    "www.domain.com",
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 SECURE_SSL_REDIRECT = True
 
