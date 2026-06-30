6761af23e0f56c8ec435c034a04455c24a816511
c0mm0s <multicolorr847@gmail.com>
2026-06-30 11:06:04 +0330

    Update urls.py

--- Arsha/urls.py
+++ Arsha/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
-import debug_toolbar
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -40,7 +39,6 @@ urlpatterns = [
         name="django.contrib.sitemaps.views.sitemap",
     ),
     path('robots.txt', include('robots.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
     path('summernote/', include('django_summernote.urls')),
     path('captcha/', include('captcha.urls')),
 ]
