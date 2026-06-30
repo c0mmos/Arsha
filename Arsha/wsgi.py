1231169521e1720d4ad992b26d54a37194787a77
c0mm0s <multicolorr847@gmail.com>
2026-06-30 13:58:03 +0330

    Update wsgi.py

--- Arsha/wsgi.py
+++ Arsha/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Arsha.settings.prod')
+
+try:
+    call_command('collectstatic', interactive=False, verbosity=0)
+except:
+    pass  # Don't fail if collectstatic fails
 
 application = get_wsgi_application()
