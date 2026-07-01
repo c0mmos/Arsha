af242f29487f8ad1aca361104c23e085ff715aac
c0mm0s <multicolorr847@gmail.com>
2026-07-01 11:58:00 +0330

    .

--- Arsha/settings/base.py
+++ Arsha/settings/base.py
@@ -130,9 +130,16 @@ WSGI_APPLICATION = 'Arsha.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': "/database/db.sqlite3",
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
