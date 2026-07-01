fca6f00022c9a36d24114b9f848de3f069e8ce7e
c0mm0s <multicolorr847@gmail.com>
2026-07-01 15:57:06 +0330

    ...

--- Arsha/settings/base.py
+++ Arsha/settings/base.py
@@ -130,14 +130,9 @@ WSGI_APPLICATION = 'Arsha.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-        "CONN_MAX_AGE": 60,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': "/database/db.sqlite3",
     }
 }
 
